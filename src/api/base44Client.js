af5dbe856ff38dc6d98f59dd8b78441f6f7e78e9
dancap03 <146107009+Dancap03@users.noreply.github.com>
2026-05-07 21:22:05 +0200

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -4,10 +4,5 @@ import { appParams } from '@/lib/app-params';
 const { appId, serverUrl, token, functionsVersion } = appParams;
 
 //Create a client with authentication required
-export const base44 = createClient({
-  appId,
-  serverUrl,
-  token,
-  functionsVersion,
-  requiresAuth: false
-});
+export const base44 = {
+};

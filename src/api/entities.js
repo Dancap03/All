4bcfb05401ea4b295acc93802833c3c15b2d18c8
dancap03 <146107009+Dancap03@users.noreply.github.com>
2026-05-07 21:29:45 +0200

    Update entities.js

--- src/api/entities.js
+++ src/api/entities.js
@@ -1,11 +1,4 @@
-import { db } from './firebase';
-
-// Por ahora dejamos estas funciones vacías para que la web cargue. 
-// Más adelante las conectaremos a tus tablas de Firebase.
-export const Query = () => ({
-  execute: async () => []
-});
-
+export const Query = () => ({ execute: async () => [] });
 export const User = {};
 export const GymRecord = {};
 export const FinanceTransaction = {};

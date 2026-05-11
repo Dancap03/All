5045a9759441dad555b555ee3bbd75ed32260e95
dancap03 <146107009+Dancap03@users.noreply.github.com>
2026-05-11 20:11:51 +0200

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -10,8 +10,7 @@ const firebaseConfig = {
   storageBucket: "dancaballinone.firebasestorage.app",
   messagingSenderId: "375365819039",
   appId: "1:375365819039:web:b72ff45bfadc8ff9a520aa",
-  // measurementId eliminado: evita que Firebase cargue Analytics/moment.js
-  // que provoca el warning de updateLocale y puede causar errores de inicializacion
+  measurementId: "G-QWN7NMGEJF"
 };
 
 const app = initializeApp(firebaseConfig);

2a9e6f945cbc4ac586ff8d3ced84ceb7853a130d
dancap03 <146107009+Dancap03@users.noreply.github.com>
2026-05-11 15:38:44 +0200

    Update firebase.js

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
+ 
 const firebaseConfig = {
   apiKey: "AIzaSyCtfDrJwWbmE7m9OwwHxSJwAiIvkr1GKTU",
   authDomain: "dancaballinone.firebaseapp.com",
@@ -9,9 +9,10 @@ const firebaseConfig = {
   storageBucket: "dancaballinone.firebasestorage.app",
   messagingSenderId: "375365819039",
   appId: "1:375365819039:web:b72ff45bfadc8ff9a520aa",
-  measurementId: "G-QWN7NMGEJF"
+  // measurementId eliminado: evita que Firebase cargue Analytics/moment.js
+  // que provoca el warning de updateLocale y puede causar errores de inicializacion
 };
-
+ 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
